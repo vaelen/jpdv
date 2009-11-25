96f105e95bae31fbb297b8a41f77e2f131d31e3b
Andrew Young <vaelen@moka.(none)>
2009-11-25 13:58:22 -0600

    Working in progress

--- src/org/vaelen/jpdv.clj
+++ src/org/vaelen/jpdv.clj
@@ -5,13 +5,25 @@ Sample clojure source file
 (ns org.vaelen.jpdv
   (:use ([clojure.xml :only (parse)])) 
   (:import (java.io.File))
-    (:gen-class))
-
+    (:gen-class)
+)
 
 (defn parse-cabocha 
   "Parses the given XML file in CaboCha format.
    If no file name is given, an example is parsed."
-  ([] (parse-cabocha "examples/utf-8/keio_st_overview/overview.xml"))
-  ([filename] (parse (java.io.File. filename)))
+  ([] (parse-cabocha "/home/vaelen/projects/jpdv/examples/utf-8/keio_st_overview/overview.xml"))
+  ([filename] (clojure.xml/parse (java.io.File. filename)))
+)
+(defn do-map-sentences 
+  [sentences] (
+    (println (first sentences))
+    (if(seq(rest sentences)) (do-map-sentences (rest sentences)))
+  )
+)
+
+(defn load-cabocha-file
+  " Parses the given file and returns a nicer data structure."
+  ([] (do-map-sentences(parse-cabocha)))
+  ([filename] (do-map-sentences parse-cabocha(filename)))
 )
 
