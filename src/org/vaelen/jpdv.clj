4831cee627f4733497f62f3a4a0478858d9ff5de
Andrew Young <vaelen@vaelen-laptop.(none)>
2009-11-28 05:59:02 -0600

    Working on generating the word context.

--- src/org/vaelen/jpdv.clj
+++ src/org/vaelen/jpdv.clj
@@ -24,18 +24,57 @@ Sample clojure source file
 (defn increment-count [map key]
   (assoc map key (+ 1 (get map key 0))))
 
-(defn do-map-sentences [sentences]
+(defn manage-previous-context 
+  "Manages a previous-context buffer, adding tokens to it while maintaining a size no larger than the
+   context-size"
+  [context context-size token]
+  (conj context token))
+
+(defn manage-next-context 
+  "Manages a next-context buffer, adding tokens to it while maintaining a size no larger than the
+   context-size"
+  [context context-size token]
+  (conj context token))
+
+(defn update-context
+  "Merges an existing previous-context and next-context with new ones if necessary."
+  [old-record new-record]
+  (if (nil? old-record)
+    new-record ; No merge needed
+    ({
+      ; Add the values together to get a total count.
+      :token (:token old-record)
+      :prev (merge-with #(+ %1 %2) (:prev old-record) (:prev new-record)) 
+      :next (merge-with #(+ %1 %2) (:next old-record) (:next new-record)) 
+      :count (+ (:count old-record) (:count new-record))
+     })))
+
+(defn get-context [sentences context-size]
   (let [t (text sentences)]
-    (loop [types {} token (first t) txt (rest t)]
-;      (println token)
+    (loop [types {} prev-context [] next-context [] token (first t) txt (rest t)]
       (if (empty? txt)
-	(increment-count types token)
-	(recur (increment-count types token) (first txt) (rest txt))))))
+	(assoc types token (update-context (types token) {
+							  :prev prev-context 
+							  :next next-context
+							  :token token
+							  :count 1
+							 }))
+	(recur 
+	 (assoc types token (update-context (types token) {
+							   :prev prev-context 
+							   :next next-context
+							   :token token
+							   :count 1
+							   }))
+	 (manage-previous-context prev-context context-size token)
+	 ;(manage-next-context next-context context-size token)
+	 next-context
+	 (first txt) (rest txt))))))
 
 
 (defn load-cabocha-file
   " Parses the given file and returns a nicer data structure."
-  ([] (do-map-sentences (parse-cabocha)))
-  ([filename] (do-map-sentences (parse-cabocha(filename))))
+  ([] (get-context (parse-cabocha) 5))
+  ([filename] (get-context (parse-cabocha(filename) 5)))
 )
 
